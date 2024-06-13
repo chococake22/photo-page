0714a2e3ebb7801cec2bf54540e86d2e6d1e902a
chococake <fightest21@gmail.com>
2024-06-13 23:09:05 +0900

    modal 초안

--- src/components/Photo/PhotoGrid.js
+++ src/components/Photo/PhotoGrid.js
@@ -18,11 +18,10 @@ const PhotoDetail = (props) => {
   const [opened, setOpened] = useState(false)
 
   const clickImg = () => {
+    inputRef.current.openCloseModal();
     if(!opened) {
-      inputRef.current.openModal();
       setOpened(true)
     } else {
-      inputRef.current.closeModal();
       setOpened(false)
     }
   }
@@ -40,7 +39,9 @@ const PhotoDetail = (props) => {
           </div>
           {/* <input ref={inputRef} />
           <button onClick={focus}>집중</button> */}
+          <div className="fixed left-0 top-0">
             <ImageBig ref={inputRef} type={"aagsdg"} url={props.item.url} />
+          </div>
           <div className="w-[65%]">
             <p className="text-base sm:text-lg md:text-xl lg:text-2xl p-2 hover:font-bold">{props.item.name}</p>
           </div>
@@ -88,31 +89,52 @@ const ImageBig = forwardRef( (props, ref) => {
 
   const [modalOpen, setModalOpen] = useState(false);
 
-  console.log(props.url)
-
+  // 부모 컴포넌트에서 자식 컴포넌트를 호출하는 경우 사용함
   useImperativeHandle(ref, () => ({
-    openModal, closeModal
+    openCloseModal
   }));
 
-  const openModal = () => {
-    console.log("open")
-    setModalOpen(true);
+  const openCloseModal = () => {
+    if(modalOpen) {
+      console.log("close")
+      setModalOpen(false);
+    } else {
+      console.log("open")
+      setModalOpen(true);
+    }
   };
 
-  const closeModal = () => {
-    console.log("close")
-    setModalOpen(false);
-  };
 
-  
   return (
-    <div>
-      {modalOpen && <img src={props.url} className="w-100 h-100"/>}
-    </div>
+  <>
+    {modalOpen &&
+       <div id="static-modal" data-modal-backdrop="static" className="fixed inset-0 overflow-y-auto overflow-x-hidden z-50 justify-center flex items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-opacity-50">
+          <div className="relative p-4 w-full max-w-xl max-h-full">
+              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                  <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                      <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                          자세히 보기
+                      </h3>
+                      <button type="button" onClick={() => openCloseModal()} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span className="sr-only">Close modal</span>
+                      </button>
+                  </div>
+                  <div className="p-4 md:p-5 space-y-4">
+                    <img src={props.url} className="w-full h-full"/>
+                  </div>
+                  <div className="grid justify-items-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                      <button data-modal-hide="static-modal" onClick={() => openCloseModal()} type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">닫기</button>
+                  </div>
+              </div>
+          </div>
+      </div>}
+    </>
   )
 });
   
-
 export { ImageBig };
 
 
